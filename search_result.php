e4e4d95af6f12ac46ff89b93ebf95194a1463062
suhkyung12 <suhkyung12@gmail.com>
2017-12-07 17:37:30 +0900

    search 날짜 완료 코드 더러운데 고칠 힘x.....

--- search_result.php
+++ search_result.php
@@ -2,59 +2,116 @@
 
 $conn = mysqli_connect('localhost','lantern','lantern','lantern');
 $search = $_GET['keyword'];
+$date = $_GET['date'];
 $search_list = array();
 $places = array();
+echo($date);
 
 $kidquery = "SELECT * FROM `keyword` WHERE `keyword` LIKE '%$search%'";//kid찾기
 $result1 = mysqli_query($conn, $kidquery);
 $i=0;
+
 while ($row1 = mysqli_fetch_assoc($result1)){
-    $i++;
     echo ("  kid: ".$row1['kid']);
-    $pidquery="SELECT * FROM `pkrelation` WHERE `kid` = ".$row1['kid']." ORDER BY `super_offset` DESC";//pid 찾기
+    $pidquery = "SELECT * FROM `pkrelation` WHERE `kid` = ".$row1['kid']." ORDER BY `super_offset` DESC";//pid 찾기
     $result2 = mysqli_query($conn, $pidquery);
     echo "  pid들: ";
     while($row2 = mysqli_fetch_assoc($result2)){
-        echo (" ".$row2['pid']." ");
-        $sidquery="SELECT * FROM `posting` WHERE `pid` = ".$row2['pid'];//sid 찾기
-        $result3 = mysqli_query($conn, $sidquery);
-        $row3 = mysqli_fetch_assoc($result3);
+        if($date!=null) {//날짜 입력 시
+            $calquery = "SELECT * FROM `pcalendar` WHERE `available_dates` LIKE '%$date%'";
+            $calresult = mysqli_query($conn, $calquery);
+            $cal = mysqli_num_rows($calresult);
+            if ($cal != 0) {//해당 날짜가 available dates에 있으면 다음 쿼리 진행
+                echo(" " . $row2['pid'] . " ");
+                $sidquery = "SELECT * FROM `posting` WHERE `pid` = " . $row2['pid'];//sid 찾기
+                $result3 = mysqli_query($conn, $sidquery);
+                $row3 = mysqli_fetch_assoc($result3);
 
-        $memquery = "SELECT * FROM `member` WHERE `sid` = ".$row3['lantern_sid'];
-        $result4 = mysqli_query($conn, $memquery);
-        $row4 = mysqli_fetch_assoc($result4);
+                $memquery = "SELECT * FROM `member` WHERE `sid` = " . $row3['lantern_sid'];
+                $result4 = mysqli_query($conn, $memquery);
+                $row4 = mysqli_fetch_assoc($result4);
 
-        $requery = "SELECT * FROM `review` WHERE `receiver_sid` = ".$row3['lantern_sid'];
-        $reresult = mysqli_query($conn, $requery);
-        $reresult2 = mysqli_query($conn, $requery);
-        $reviewscounter = mysqli_num_rows($reresult);
-        if($reviewscounter!=0){
-            $total = 0;
-            while ($row5 = mysqli_fetch_assoc($reresult2)) {
-                $total = $total + $row5['rate'];
+                $requery = "SELECT * FROM `review` WHERE `receiver_sid` = " . $row3['lantern_sid'];
+                $reresult = mysqli_query($conn, $requery);
+                $reresult2 = mysqli_query($conn, $requery);
+                $reviewscounter = mysqli_num_rows($reresult);
+                if ($reviewscounter != 0) {
+                    $total = 0;
+                    while ($row5 = mysqli_fetch_assoc($reresult2)) {
+                        $total = $total + $row5['rate'];
+                    }
+                    $averagescore = $total / $reviewscounter;
+                    $averagescore = round($averagescore);
+                }
+
+                array_push($search_list, array(
+                    "pid" => $row2['pid'],
+                    "sid" => $row3['lantern_sid'],
+                    "name" => $row4['name_first'] . " " . $row4['name_last'],
+                    "reviewscounter" => $reviewscounter,
+                    "rate" => $averagescore,
+                    "keyword" => $row1['keyword']
+                ));
+
+                if ($row1['geo_offset'] == 1) {
+                    array_push($places, array(
+                        kid => $row1['kid'],
+                        keyword => $row1['keyword'],
+                        location => $row1['geo_location'],
+                        address => $row1['geo_address'],
+                        name => $row1['geo_name']
+                    ));
+                }
+                $i++;
+
             }
-            $averagescore = $total / $reviewscounter;
-            $averagescore = round($averagescore);
-        }
+        }else{//날짜 미입력 시 전체검색
+            echo (" ".$row2['pid']." ");
+            $sidquery = "SELECT * FROM `posting` WHERE `pid` = ".$row2['pid'];//sid 찾기
+            $result3 = mysqli_query($conn, $sidquery);
+            $row3 = mysqli_fetch_assoc($result3);
 
-        array_push($search_list,array(
-            "pid" => $row2['pid'],
-            "sid" => $row3['lantern_sid'],
-            "name" => $row4['name_first']." ".$row4['name_last'],
-            "reviewscounter" => $reviewscounter,
-            "rate" => $averagescore,
-            "keyword" => $row1['keyword']
-        ));
+            $memquery = "SELECT * FROM `member` WHERE `sid` = ".$row3['lantern_sid'];//멤버정보 찾기
+            $result4 = mysqli_query($conn, $memquery);
+            $row4 = mysqli_fetch_assoc($result4);
 
-        if($row1['geo_offset']==1){
-            array_push($places,array(
-                kid => $row1['kid'],
-                keyword => $row1['keyword'],
-                location=>$row1['geo_location'],
-                address=>$row1['geo_address'],
-                name=> $row1['geo_name']
+            $requery = "SELECT * FROM `review` WHERE `receiver_sid` = ".$row3['lantern_sid'];//리뷰찾기
+            $reresult = mysqli_query($conn, $requery);
+            $reresult2 = mysqli_query($conn, $requery);
+            $reviewscounter = mysqli_num_rows($reresult);
+            if($reviewscounter!=0){
+                $total = 0;
+                while ($row5 = mysqli_fetch_assoc($reresult2)) {
+                    $total = $total + $row5['rate'];
+                }
+                $averagescore = $total / $reviewscounter;
+                $averagescore = round($averagescore);
+            }
+
+            array_push($search_list,array(
+                "pid" => $row2['pid'],
+                "sid" => $row3['lantern_sid'],
+                "name" => $row4['name_first']." ".$row4['name_last'],
+                "reviewscounter" => $reviewscounter,
+                "rate" => $averagescore,
+                "keyword" => $row1['keyword']
             ));
+
+            if($row1['geo_offset']==1){
+                array_push($places,array(
+                    kid => $row1['kid'],
+                    keyword => $row1['keyword'],
+                    location=>$row1['geo_location'],
+                    address=>$row1['geo_address'],
+                    name=> $row1['geo_name']
+                ));
+            }
+            $i++;
+
         }
+
+
+
     }
     echo "  /////  ";
 }
